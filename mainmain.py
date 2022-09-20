2c6b40edaa8d0e487b73f155811f1cfa8e84af43
dulda280 <Sebber280@live.dk>
2022-09-20 15:22:05 +0200

    Attempted manual optical flow

--- mainmain.py
+++ mainmain.py
@@ -1,12 +1,18 @@
+from re import A
+from telnetlib import AYT
+from turtle import left
 import cv2 as cv
 import numpy as np
 import images.kmeans
-from skimage.feature import hog
-from skimage.feature import match_template
+#from skimage.feature import hog
+#from skimage.feature import match_template
 import sys
-import numpy
+import math
 
-numpy.set_printoptions(threshold=sys.maxsize)
+
+
+
+np.set_printoptions(threshold=sys.maxsize)
 
 
 def compareMovement(frame, frames):
@@ -20,25 +26,25 @@ def compareMovement(frame, frames):
 
 
 def regionCannyEdge(frame):
-    canny = cv.Canny(frame, threshold1=100, threshold2=200)
+    canny = cv.Canny(frame, threshold1=50, threshold2=200, apertureSize = 3, L2gradient = True)
 
     return canny
 
 
-def hoggers(frameGray):
-    # frameGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    hogGrad, hogIMG = hog(frameGray, orientations=8, pixels_per_cell=(16, 16),
-                          cells_per_block=(1, 1), visualize=True, channel_axis=-1)
+# def hoggers(frameGray):
+#     # frameGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+#     hogGrad, hogIMG = hog(frameGray, orientations=8, pixels_per_cell=(16, 16),
+#                           cells_per_block=(1, 1), visualize=True, channel_axis=-1)
 
-    # print(hogGrad)
+#     # print(hogGrad)
 
-    return hogGrad, hogIMG
+#     return hogGrad, hogIMG
 
 
-def templateTracking(template, frame):
-    templateimage = match_template(frame, template, pad_input=False)
+# def templateTracking(template, frame):
+#     templateimage = match_template(frame, template, pad_input=False)
 
-    return templateimage
+#     return templateimage
 
 
 def tomasiTacking(frame):
@@ -67,14 +73,14 @@ def edgeDiffTracking(frame):
 
     x, y, x1, y1 = np.asarray(x), np.asarray(y), np.asarray(x1), np.asarray(y1)
 
-    print(f'x, y, x1, y1: {len(x)}, {len(y)}, {len(x1)}, {len(y1)}')
+    #print(f'x, y, x1, y1: {len(x)}, {len(y)}, {len(x1)}, {len(y1)}')
 
     for i in range(len(x1)):
         for w in range(len(x)):
             if x[w] == x1[i] and y[w] == y1[i]:
                 cv.circle(frame, (x[w], x[w]), 10, (255, 255, 255), 10)
-                cv.imwrite(f'markedImg/cirlcedImage{i}.png', frame)
-                print('Img taken')
+                #cv.imwrite(f'markedImg/cirlcedImage{i}.png', frame)
+                #print('Img taken')
 
 
 
@@ -91,22 +97,106 @@ def edgeDiffTracking(frame):
     cv.imshow('diffCircles', frame)
     return cannyImg
 
+def regionBasedDetection(frame, leftArrX, leftArrY):
+    leftRegion = int(frame.shape[1]*0.50)
+    rightRegion = int(frame.shape[1])
+    
+    
+    canny = regionCannyEdge(frame)
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame = cv.GaussianBlur(frame, (5,5), 0)
+    
+    cv.imshow('framegray', frame)
+    SobelX = cv.Sobel(frame, ddepth=cv.CV_8U, dx=1, dy=0, ksize=5)
+    SobelY = cv.Sobel(frame, ddepth=cv.CV_8U, dx=0, dy=1, ksize=5)
+
+
+    # cv.rectangle(canny, (0, 0), (leftRegion, frame.shape[0]), (255, 0, 0), 3)
+    # cv.rectangle(canny, (leftRegion, 0), (rightRegion, frame.shape[0]), (255), 3)
+
+    leftRegionCannyIMG = canny[0:canny.shape[0], 0:int(canny.shape[1]/2)]
+    rightRegionCannyIMG = canny[0:canny.shape[0], int(canny.shape[1]/2):canny.shape[1]]
+    cv.imshow('leftcanny', leftRegionCannyIMG)
+    cv.imshow('rightcanny', rightRegionCannyIMG)
+
+    leftRegionIMG = frame[0:canny.shape[0], 0:int(canny.shape[1]/2)]
+    rightRegionIMG = frame[0:canny.shape[0], int(canny.shape[1]/2):canny.shape[1]]
+    cv.imshow('left', leftRegionIMG)
+    cv.imshow('right', rightRegionIMG)
+
+    rightRegSobelX = SobelY[0:canny.shape[0], int(canny.shape[1]/2):canny.shape[1]]
+    rightRegSobelY = SobelX[0:canny.shape[0], int(canny.shape[1]/2):canny.shape[1]]
+
+    leftRegSobelX = SobelX[0:canny.shape[0], 0:int(canny.shape[1]/2)]
+    leftRegSobelY = SobelY[0:canny.shape[0], 0:int(canny.shape[1]/2)]
+
+    leftArrX.append(leftRegSobelX)
+    leftArrY.append(leftRegSobelY)
+
+    OFimgarray = np.zeros_like(leftRegSobelX)
+    if len(leftArrX) > 1:
+        Ax = leftArrX[0]
+        Bx = leftArrX[1]
+        Cx = cv.subtract(Bx, Ax)
+
+        Ay = leftArrY[0]
+        By = leftArrY[1]
+        Cy = cv.subtract(By, Ay)
+
+        cv.imshow('cx', Cx)
+        cv.imshow('cy', Cy)
+
+        
+        for i in range(0, Ax.shape[0]):
+            for j in range(0, Ax.shape[1]):
+                gradientMatrix = [[(Ax[i][j])**2, (Ax[i][j])*(Ay[i][j])], [(Ax[i][j])*(Ay[i][j]), (Ay[i][j])**2]]
+
+                #invGradMatrix = np.linalg.inv(gradientMatrix)
+                changeMatrix = [-((Ax[i][j])*(Cx[i][j])),-((Ay[i][j])*(Cy[i][j]))]
+                
+                OFvector = np.matmul(gradientMatrix,changeMatrix)
+                #print('of', OFvector)
+                ##OFimgarray[i][j] = OFvector
+
+
+                
+
+                
+                
+        
+        
+        leftArrX.clear()
+        leftArrY.clear()
+        return canny
+        
+    
+
+    cv.waitKey(0)
+    
+    return canny
+
 
 
 if __name__ == '__main__':
-    cap = cv.VideoCapture('data/trimVideo.mp4')
+    #cap = cv.VideoCapture('data/trimVideo.mp4')
+    cap = cv.VideoCapture('E:/MED-local/MED7/MED7_TrashNet_Datacollection/data/trimVideo.mp4')
     templateIMG = cv.imread('data/template.png')
     print("open  = ", cap.isOpened())
     frames = []
+    rightRegionFrames = []
+    leftRegionFrames = []
     while True:
 
         ret, frame = cap.read()
         croppedIMG = frame[int(frame.shape[0] * 0.40):frame.shape[0], 0:frame.shape[1]]
 
         diff = compareMovement(croppedIMG, frames)
+        regioIMG = regionBasedDetection(croppedIMG, leftRegionFrames, rightRegionFrames)
+        
+        #v.imshow('region', regioIMG)
 
 
-
+        
 
         imggg = edgeDiffTracking(croppedIMG)
 
@@ -121,11 +211,15 @@ if __name__ == '__main__':
             pass
 
         canny = regionCannyEdge(croppedIMG)
-        cv.imshow('canny', canny)
-        cv.imshow('video', croppedIMG)
+        # cv.imshow('canny', canny)
+        # cv.imshow('video', croppedIMG)
         # kmeans = images.kmeans.kMeansSegmentation(frame)
         # cv.imshow('kmeans')
+
+        
 
         keyboard = cv.waitKey(30)
         if keyboard == 'q' or keyboard == 27:
             break
+
+       
